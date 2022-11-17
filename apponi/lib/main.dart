6b543eb4437819a84e59b5b2568dff4887ba96ae
symphonycellis <symphony.cellis@ui.ac.id>
2022-11-17 13:37:11 +0700

    Mengerjakan lab 8

--- apponi/lib/main.dart
+++ apponi/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:apponi/form.dart';
+import 'package:apponi/page/form.dart';
 import 'package:flutter/material.dart';
+import 'package:apponi/page/to_do_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -77,6 +79,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('ToDo'),
+              onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
                 );
               },
             ),
